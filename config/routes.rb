415bf719db8ea668346431b52bcd0532398950d1
wesleymorlock <wjm024@bucknell.edu>
2018-08-18 13:10:24 -0700

    Add App and Budget controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'app/index'
+
+  resources :budgets
+
+  root 'app#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
